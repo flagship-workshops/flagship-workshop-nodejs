8ed3fbdb865b7d3c0fb26c7d096a341060302d44
Micah Silverman <micah@afitnerd.com>
2021-02-26 12:24:22 -0500

    added cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require("cors");
 const bodyParser = require('body-parser');
 const movies = require('./movies');
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
